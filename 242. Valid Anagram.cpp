6feade12aed1d409842ada86f77ade684af4d142
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2020-10-09 11:05:16 +0530

    Create 242. Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n1=s.size();
+        int n2=t.size();
+        if(n1!=n2)
+        {
+            return false;
+        }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<n1;i++)
+        {
+            if(s[i]!=t[i])
+            {
+                return false;
+            }
+        }
+       return true; 
+    }
+};
