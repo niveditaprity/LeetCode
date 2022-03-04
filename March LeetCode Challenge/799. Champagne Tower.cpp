74ad5f4c12fcd1ce189cb618174f0d0acc98deb7
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2022-03-04 15:58:07 +0530

    Update 799. Champagne Tower.cpp

--- March LeetCode Challenge/799. Champagne Tower.cpp
+++ March LeetCode Challenge/799. Champagne Tower.cpp
@@ -1,25 +1,30 @@
+
+
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        if(poured==0)
+      
+       if(poured==0)
+       {
+           return 0;
+       }
+        vector<double>list;
+        list.push_back((double)poured);
+        for(int i=1;i<=query_row;i++)
         {
-            return 0;
-        }
-        vector<double> curr;
-        curr.push_back((double) poured); 
-        for(int i=1; i<=query_row; i++)
-        {
-             vector<double>temp;
-            temp.push_back(max((curr[0]-1)/2,0.0));
-            for(int j=1; j<curr.size(); j++)
+            
+            vector<double>temp;
+            temp.push_back(max((list[0]-1)/2,0.0));
+            for(int j=1;j<list.size();j++)
             {
-                
-              temp.push_back(max((curr[j]-1)/2, 0.0)  + max((curr[j-1]-1)/2,0.0));
+                temp.push_back(max((list[j]-1)/2,0.0) + max((list[j-1]-1)/2,0.0));
             }
             temp.push_back(temp[0]);
-            curr=temp;
- 
+            list=temp;
         }
-        return min( 1.0, curr[query_glass]);
+        return min(1.0,list[query_glass]);
+     
     }
-};
+};        
+
+ 
