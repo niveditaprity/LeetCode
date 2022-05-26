7fe59931fa11669548cd7f255342374bb8c6c5df
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2022-05-26 10:13:52 +0530

    Create Day26.cpp

---
+++ May Leetcode Challenge/Day26.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+         int count=0;
+        while(n>0)
+        {
+           count+=(n&1);
+            n>>=1;
+        }
+        return count;
+    }
+};
