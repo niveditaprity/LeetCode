b375e2c725fd33d2f72e6a2931e68d87852d2f15
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2022-07-04 16:25:30 +0530

    135. Candy

---
+++ July Leetcode Challenge 2022/Day4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+      int n=ratings.size();
+	   vector<int>candy(n,1);
+		for(int i=1;i<n;i++)
+		{
+		if(ratings[i]>ratings[i-1] && candy[i]<=candy[i-1])
+				candy[i]=candy[i-1]+1;
+		}
+		for(int i=n-2;i>=0;i--)
+		{
+		if(ratings[i]>ratings[i+1] && candy[i]<=candy[i+1])
+				candy[i]=candy[i+1]+1;
+		}
+		return accumulate(candy.begin(),candy.end(),0);  
+    }
+};
