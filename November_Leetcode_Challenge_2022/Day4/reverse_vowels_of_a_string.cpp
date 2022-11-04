3149508899784737e6b947c9df5481127ddb6e44
Nivedita kumari <44724151+niveditaprity@users.noreply.github.com>
2022-11-04 09:50:12 +0530

    Create reverse_vowels_of_a_string.cpp

---
+++ November_Leetcode_Challenge_2022/Day4/reverse_vowels_of_a_string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'i' || c == 'e' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'I' || c == 'E' || c == 'O' || c == 'U';
+    }
+    string reverseVowels(string s) {
+      int i = 0,j=s.size()-1;
+      while(i<j)
+      {
+          if(isVowel(s[i]) && isVowel(s[j]))
+          {
+              swap(s[i],s[j]);
+              i++;
+              j--;
+          }
+          else if(!isVowel(s[i]))
+          {
+            i++;  
+          }
+          else if(!isVowel(s[j]))
+          {
+              j--;
+          }
+      }
+        return s;
+    }
+};
